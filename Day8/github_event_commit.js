a8efe704922372041d8b855cae2e7b209a66ea8e
unknown <meiltokalicharan@gmail.com>
2022-11-04 18:59:30 +0530

    to create a event commit

---
+++ Day8/github_event_commit.js
@@ -0,0 +1,47 @@
+function getEventCommit(){
+    alert("hi");
+    document.getElementById('eventCommit').style.display="block";
+   const userCommit = document.getElementById('username').value;    
+   console.log("name" + userCommit);
+    fetch(`https://api.github.com/users/${userCommit}/events/public`)
+    .then(response=>response.json()
+        .then(data=>{
+         
+        console.log(data);
+        let githubEventCommit ="";
+
+        for(let i = 0; i < data.length; i++){
+
+          const {commits} = data[i].payload;
+
+        for (let j = 0; j < commits.length; j++) {
+           
+                      console.log(commits[j]);
+
+         let author =
+          githubEventCommit += ` <div class="output-container" id="output" style="display: block;">
+                                <div class="profile" id="profile">
+                                    <img src="${data[i].actor.avatar_url}">
+                                </div>
+                                <div class="reponame">
+                                    <h1 id="name">${commits[j].sha}</h1>
+                                </div>
+                                <div class="bio">
+                                   <p id="bio">${commits[j].author.name}</p>
+                               </div>
+                               <div class="info">
+                                   <ul>
+                              <li> ${commits[j].message} </li>
+                                 </ul>     
+                                   
+                               </div>
+                               </div>
+                                                         
+            `
+          }
+        }
+          document.getElementById('eventCommit').innerHTML= githubEventCommit;
+          
+        
+    }))
+}
