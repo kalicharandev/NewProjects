9306a7cfd61e9222d58885552af149788b57fc80
unknown <meiltokalicharan@gmail.com>
2022-11-19 21:07:16 +0530

    to create a drink water level

--- Day15/drinkwater.js
+++ Day15/drinkwater.js
@@ -1,24 +1,50 @@
 let result =  document.getElementById("value");
-let count = 0
+let count = 0;
+ let isWater1Selcted = false;
+ let isWater2Selected = false;
+ let isWater3Selcted = false;
+ let isWater4Selcted = false;
 
-function getWater1(){
-let water1 = document.getElementById("water1");
-count += 1;
+function getWaterLevel(index){
+
+console.log(index);
+if(isWater1Selcted && index == 1){
+   count--;
+    result.innerHTML = count;
+    isWater1Selcted = false;
+   console.log(`count decrement" + ${count} + isWater1Selcted - ${isWater1Selcted}`);
+}else if(!isWater1Selcted && index == 1){
+    count++;
+    isWater1Selcted = true;
+    console.log(`count increment" + ${count} + isWater1Selcted - ${isWater1Selcted}`);
+} else if(isWater2Selected && index == 2){
+   count--;
+   isWater2Selected = false;
+   result.innerHTML = count;
+   console.log(`count decrement" + ${count} + isWater2Selcted - ${isWater2Selected}`);
+}else if(index == 2 && !isWater2Selected){
+    count++;
+    isWater2Selected = true;
+    console.log(`count increment" + ${count} + isWater2Selcted - ${isWater2Selected}`);
+}else if(isWater3Selcted && index == 3){
+    count--;
+    isWater3Selcted = false;
+   console.log(`count decrement" + ${count} + is  isWater3Selcted -${isWater3Selcted}`);
+}else if(!isWater3Selcted && index ==3){
+    count++;
+    isWater3Selcted = true;
+    console.log(`count increment" + ${count} + isWater3Selcted- ${isWater4Selcted}`);
+}else if(isWater4Selcted && index == 4 ){
+    count--;
+    isWater4Selcted = false;
+   console.log(`count decrement" + ${count} + is  isWater3Selcted -${isWater4Selcted}`);
+}else if(!isWater4Selcted && index == 4){
+    count++;
+    isWater4Selcted = true;
+    console.log(`count increment" + ${count} + isWater3Selcted- ${isWater4Selcted}`);
+
+}
+
+
 result.innerHTML = count;
-}
-function getWater2(){
-    let water2 = document.getElementById("water2");
-    count += 1;
-    result.innerHTML = count;
-}
-function getWater3(){
-    let water3 = document.getElementById("water3");
-    count += 1;
-    result.innerHTML = count;
-}
-function getWater4(){
-    let water4 = document.getElementById("water4");
-    count += 1;
-    result.innerHTML = count;
-
 }
